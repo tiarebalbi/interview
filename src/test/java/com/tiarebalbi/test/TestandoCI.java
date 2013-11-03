c1b13f05eb01b2a65c0a86ee115645ed00eb43c2
Tiarê Balbi Bonamini <tiarebalbi@me.com>
2013-11-03 16:16:31 -0200

    Teste e configuração do Pom

---
+++ src/test/java/com/tiarebalbi/test/TestandoCI.java
@@ -0,0 +1,14 @@
+package com.tiarebalbi.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestandoCI {
+
+	@Test
+	public void deveTestarCIComAmbienteDeDesenvolvimento() {
+		assertEquals("aa", "aa");
+	}
+
+}
